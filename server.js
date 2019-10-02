a9c1468ce7054a90e5cf501420c6c2b6a4e6b8c3
Chris Son <chris.son@mediavalet.com>
2019-10-01 19:00:06 -0700

    setting up heroku db

--- server.js
+++ server.js
@@ -10,24 +10,24 @@ const cors = require('cors');  // allows/disallows cross-site communication
 const morgan = require('morgan'); // logs requests
 
 // db Connection w/ Heroku
-// const db = require('knex')({
-//   client: 'pg',
-//   connection: {
-//     connectionString: process.env.DATABASE_URL,
-//     ssl: true,
-//   }
-// });
+ const db = require('knex')({
+   client: 'pg',
+   connection: {
+     connectionString: process.env.DATABASE_URL,
+     ssl: true,
+   }
+ });
 
 // db Connection w/ localhost
-var db = require('knex')({
-  client: 'pg',
-  connection: {
-    host : '127.0.0.1',
-    user : 'ChrisSon',
-    password : '',
-    database : 'gimbap'
-  }
-});
+//var db = require('knex')({
+//  client: 'pg',
+//  connection: {
+//    host : '127.0.0.1',
+//    user : 'ChrisSon',
+//    password : '',
+//    database : 'gimbap'
+//  }
+//});
 
 // Controllers - aka, the db queries
 const main = require('./controllers/main');
